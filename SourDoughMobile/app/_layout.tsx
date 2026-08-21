984d6da80ce238431c910389fe5dcb355be09abd
Emre <125869525+tuncoglu@users.noreply.github.com>
2026-08-21 22:37:29 +0100

    Preload Ionicons font to fix blank icons

--- SourDoughMobile/app/_layout.tsx
+++ SourDoughMobile/app/_layout.tsx
@@ -1,4 +1,5 @@
 import { useFonts } from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
@@ -22,6 +23,7 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const [loaded, error] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    ...Ionicons.font,
   });
 
   useEffect(() => {
